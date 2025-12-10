1da2620dc065d71a7c95ee63a9f48462ceea0b02
deni <deni@example.de>
2025-12-10 13:51:08 +0100

    added docker support llm und server; remove .env from llm_service git

--- server/src/config/data_source.ts
+++ server/src/config/data_source.ts
@@ -1,35 +1,49 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import { Subscription } from '../entities/Subscription';
-import { Attribute } from '../entities/Attribute';
-import { UserIngredient } from '../entities/UserIngredient';
-import { Recipe } from '../entities/Recipe';
-import { Event } from '../entities/Event';
-import { Ingredient } from '../entities/Ingredient';
-import dotenv from 'dotenv';
-import { User } from '../entities/User';
-import { RecipeIngredient } from '../entities/RecipeIngredient';
-import { Category } from '../entities/Category';
-import { RecipeTag } from '../entities/RecipeTag';
-import { CustomIngredient } from '../entities/Custom_Ingredient';
-import { Unit } from '../entities/Unit';
-import { ExternalRecipe } from '../entities/ExternalRecipe';
-import { DietaryTag } from '../entities/DietaryTag';
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Subscription } from "../entities/Subscription";
+import { Attribute } from "../entities/Attribute";
+import { UserIngredient } from "../entities/UserIngredient";
+import { Recipe } from "../entities/Recipe";
+import { Event } from "../entities/Event";
+import { Ingredient } from "../entities/Ingredient";
+import dotenv from "dotenv";
+import { User } from "../entities/User";
+import { RecipeIngredient } from "../entities/RecipeIngredient";
+import { Category } from "../entities/Category";
+import { RecipeTag } from "../entities/RecipeTag";
+import { CustomIngredient } from "../entities/Custom_Ingredient";
+import { Unit } from "../entities/Unit";
+import { ExternalRecipe } from "../entities/ExternalRecipe";
+import { DietaryTag } from "../entities/DietaryTag";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'luciaschlegel',
-  password: process.env.DB_PASSWORD ,
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME || "luciaschlegel",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [ User, Ingredient, Recipe, Event, UserIngredient, Attribute, DietaryTag, Subscription, RecipeIngredient, Category, RecipeTag, CustomIngredient, Unit, ExternalRecipe ],
+  entities: [
+    User,
+    Ingredient,
+    Recipe,
+    Event,
+    UserIngredient,
+    Attribute,
+    DietaryTag,
+    Subscription,
+    RecipeIngredient,
+    Category,
+    RecipeTag,
+    CustomIngredient,
+    Unit,
+    ExternalRecipe,
+  ],
   synchronize: true, // True solo para desarrollo (genera tablas automáticamente)
   logging: false,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  //dropSchema: true,  if you want to reset the db content 
+  ssl:
+    process.env.DB_HOST === "postgres" ? false : { rejectUnauthorized: false },
+  //dropSchema: true,  if you want to reset the db content
 });
